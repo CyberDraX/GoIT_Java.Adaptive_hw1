60a0bb63729dc893866620522a6748f5f94bca37
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-06-25 18:04:21 +0300

    My first hm

---
+++ src/TreePrinter.java
@@ -0,0 +1,7 @@
+public class TreePrinter {
+    public static void main(String[] args){
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.print("*****");
+    }
+}
